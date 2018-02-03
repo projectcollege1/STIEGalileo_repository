f9ed33f22625a5b6b21552c301addf3070356a53
projectcollege1 <project.college1@gmail.com>
2018-02-03 13:24:08 +0700

    first commit

---
+++ app/src/main/java/com/example/asus/stiegalileo/Connection.java
@@ -0,0 +1,19 @@
+package com.example.asus.stiegalileo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.widget.TextView;
+
+/**
+ * Created by PCINB023 on 29/01/2018.
+ */
+
+//public class Connection extends Activity implements Runnable{
+//}
---
+++ app/src/main/java/com/example/asus/stiegalileo/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.asus.stiegalileo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by PCINB023 on 20/01/2018.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
